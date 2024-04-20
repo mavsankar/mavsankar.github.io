d917be984a0e3466abc53b01a163ff21b9cf9ade
Sankar Mantripragada <mavsankar@gmail.com>
2024-04-20 17:54:14 +0530

    Added gTag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,6 +8,25 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
       rel="stylesheet"
       href="./src/assets/academicons/css/academicons.min.css"
     />,
+    <script
+      key="gatsby-gtag-js"
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=G-YJ0E9LWS5N`}
+    />,
+    <script
+      key="gatsby-gtag-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+
+         gtag('config', 'G-YJ0E9LWS5N', {
+           page_path: window.location.pathname,
+         });
+       `
+      }}
+    />
   ]);
   setHtmlAttributes({ lang: "en" });
 };
